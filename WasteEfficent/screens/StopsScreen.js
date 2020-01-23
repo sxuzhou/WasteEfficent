7a1a614dc15a6113e5027862e8f13c0b654acb22
kraposo4 <kraposo4@uwo.ca>
2020-01-23 15:56:27 -0500

    Changes to stopsscreen and the input text boxes

--- WasteEfficent/screens/StopsScreen.js
+++ WasteEfficent/screens/StopsScreen.js
@@ -1,13 +1,7 @@
 import React, { Component } from 'react';
 import { ExpoConfigView } from '@expo/samples';
 import { ScrollView, StyleSheet, Text, TextInput, View, FlatList, Alert, TouchableOpacity} from 'react-native';
-<<<<<<< Updated upstream
-import { Button, StyleProvider } from 'native-base';
-import { FlexLeft } from '../components/FlexBox';
-import  { StopsText } from '../components/StopsText';
-=======
 import { Button, StyleProvider,Container } from 'native-base';
->>>>>>> Stashed changes
 import { InputBoxes } from '../components/InputBoxes';
 
 export default function StopsScreen() {
@@ -26,7 +20,7 @@ export default function StopsScreen() {
           <Text>Edit Stop</Text>
           </Button>
           <InputBoxes></InputBoxes>
-          <Button
+          <Button 
           onPress={() => alert("Remove stop!")}>
           <Text>Remove Stop</Text>
           </Button> 
@@ -45,10 +39,7 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingTop: 10,
     backgroundColor: '#fff',
-<<<<<<< Updated upstream
-=======
     maxWidth: '50%',
->>>>>>> Stashed changes
   },
   Button: {
     flex: 1,
