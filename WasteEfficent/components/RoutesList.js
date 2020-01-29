a033a884dd8fd34470c74fc7339724c481b47c6d
Sam Zhou <42977077+sxuzhou@users.noreply.github.com>
2020-01-29 01:37:23 -0500

    Created Stops Screen

--- WasteEfficent/components/RoutesList.js
+++ WasteEfficent/components/RoutesList.js
@@ -1,13 +1,13 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { Text } from 'react-native';
-import {ListItem, Left, Right, Radio} from 'native-base';
+import { ListItem, Left, Right, Radio } from 'native-base';
 
 export default class RoutesList extends Component {
-    render(){
-      return (
-        <ListItem
+  render() {
+    return (
+      <ListItem
         selected={this.props.selectedBoolean}
-        >
+      >
         <Left>
           <Text>{this.props.routeName}</Text>
         </Left>
@@ -20,4 +20,5 @@ export default class RoutesList extends Component {
         </Right>
       </ListItem>
     );
-}}
+  }
+}
