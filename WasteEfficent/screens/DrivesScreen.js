2a7ae835545fa981d5afa2e8397caf9e487088b7
kraposo4 <kraposo4@uwo.ca>
2020-04-01 02:56:51 -0400

    drives screen update in new branch

--- WasteEfficent/screens/DrivesScreen.js
+++ WasteEfficent/screens/DrivesScreen.js
@@ -1,10 +1,56 @@
 import React, { Component } from 'react';
 import { TextInput,Text, View } from 'react-native';
 import MapView from 'react-native-maps';
-import MyMap from '../components/MyMap'
-export default function DrivesScreen() {
-    return(
-        <MyMap></MyMap>
+import Polyline from '@mapbox/polyline';
+
+export default class RnDirectionsApp extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      coords: []
+    }
+  }
+
+  componentDidMount() {
+    // find your origin and destination point coordinates and pass it to our method.
+    // I am using Toronto, ON -> London, ON in this example below.
+    this.getDirections("43.0258, -81.2784", "43.0258, -81.2784")
+  }
+  async getDirections(startLoc, destinationLoc) {
+        try {
+            let resp = await fetch(`https://maps.googleapis.com/maps/api/directions/json?origin=${ startLoc }&destination=${ destinationLoc }&keyhere`)
+            let respJson = await resp.json();
+            let points = Polyline.decode(respJson.routes[0].overview_polyline.points);
+            let coords = points.map((point, index) => {
+                return  {
+                    latitude : point[0],
+                    longitude : point[1]
+                }
+            })
+            this.setState({coords: coords})
+            return coords
+        } catch(error) {
+            alert(error)
+            return error
+        }
+    }
+
+  render() {
+    return (
+      <View>
+        <MapView style={styles.map} initialRegion={{
+          latitude:42.9849, 
+          longitude:-81.2453, 
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421
+        }}>
+
+        <MapView.Polyline 
+            coordinates={this.state.coords}
+            strokeWidth={2}
+            strokeColor="red"/>
+        </MapView>
+      </View>
     );
 }
 DrivesScreen.navigationOptions = {
