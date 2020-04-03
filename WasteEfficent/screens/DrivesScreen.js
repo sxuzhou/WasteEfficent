f518f5c4e035f1107c56460b74f1fd26721745f2
Sam Zhou <42977077+sxuzhou@users.noreply.github.com>
2020-04-02 22:05:54 -0400

    added markers

--- WasteEfficent/screens/DrivesScreen.js
+++ WasteEfficent/screens/DrivesScreen.js
@@ -7,8 +7,10 @@ import {
   Dimensions
 } from 'react-native';
 
-import MapView from 'react-native-maps';
+import MapView, {Marker} from 'react-native-maps';
+import { Content, Card, Container } from 'native-base';
 import Polyline from '@mapbox/polyline';
+
 
 export default class RnDirectionsApp extends Component {
   constructor(props) {
@@ -24,40 +26,58 @@ export default class RnDirectionsApp extends Component {
     this.getDirections("43.0258, -81.2784", "43.0126, -81.2656")
   }
   async getDirections(startLoc, destinationLoc) {
-        try {
-            let resp = await fetch(`https://maps.googleapis.com/maps/api/directions/json?origin=${ startLoc }&destination=${ destinationLoc }&keyherefixesissues`)
-            let respJson = await resp.json();
-            let points = Polyline.decode(respJson.routes[0].overview_polyline.points);
-            let coords = points.map((point, index) => {
-                return  {
-                    latitude : point[0],
-                    longitude : point[1]
-                }
-            })
-            this.setState({coords: coords})
-            return coords
-        } catch(error) {
-            alert(error)
-            return error
+    try {
+      let resp = await fetch(`https://maps.googleapis.com/maps/api/directions/json?origin=${startLoc}&destination=${destinationLoc}&key=AIzaSyAxVOLyJLJi2xHuokJK489s_sQWM1R3DYk`)
+      let respJson = await resp.json();
+      let points = Polyline.decode(respJson.routes[0].overview_polyline.points);
+      let coords = points.map((point, index) => {
+        return {
+          latitude: point[0],
+          longitude: point[1]
         }
+      })
+      this.setState({ coords: coords })
+      return coords
+    } catch (error) {
+      alert(error)
+      return error
     }
+  }
 
   render() {
     return (
-      <View>
-        <MapView style={styles.map} initialRegion={{
-          latitude:42.9849, 
-          longitude:-81.2453, 
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421
-        }}>
-
-        <MapView.Polyline 
-            coordinates={this.state.coords}
-            strokeWidth={2}
-            strokeColor="red"/>
-        </MapView>
-      </View>
+      <Container>
+        <View>
+          <MapView style={styles.map} showsCompass={true} initialRegion={{
+            latitude: 42.9849,
+            longitude: -81.2453,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421
+          }}>
+            <MapView.Marker coordinate={{latitude: 43.025, longitude: -81.278, }}
+              title='Masonville pickup site'
+              description="Mall pickup site"
+            />
+            <MapView.Marker coordinate={{latitude: 43.020151, longitude: -81.27869, }}
+              title='Apartments 1580 pickup site'
+              description="Apartment pickup site in the back"
+            />
+            <MapView.Marker coordinate={{latitude: 43.015117, longitude: -81.270156, }}
+              title='Western Center For Public Health pickup site'
+            />
+            <MapView.Marker coordinate={{latitude: 43.017078, longitude: -81.273421, }}
+              title='Western Center For Public Health pickup site'
+            />
+            <MapView.Marker coordinate={{latitude: 43.012907, longitude: -81.265441, }}
+              title='Western Center For Public Health pickup site'
+            />
+            <MapView.Polyline
+              coordinates={this.state.coords}
+              strokeWidth={2}
+              strokeColor="red" />
+          </MapView>
+        </View>
+      </Container>
     );
   }
 }
