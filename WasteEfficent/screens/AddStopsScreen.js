7b08b75547633538e16b61201122e8831d61f802
Sam Zhou <42977077+sxuzhou@users.noreply.github.com>
2020-04-02 21:10:24 -0400

    updated Routes Screen

--- WasteEfficent/screens/AddStopsScreen.js
+++ WasteEfficent/screens/AddStopsScreen.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
+import { ActivityIndicator } from 'react-native'
 import { Container, Content, Item, Input, Button, Text, Form,} from 'native-base';
+import * as Font from 'expo-font'
 
 export default class AddStopsScreen extends Component {
     state = {
         name: '',
         address: '',
         postalCode: '',
+        loading: true
     }
-
+    componentWillMount = async () => {
+        await Font.loadAsync({
+          Roboto: require('../node_modules/native-base/Fonts/Roboto.ttf'),
+          Roboto_medium: require('../node_modules/native-base/Fonts/Roboto_medium.ttf')
+        })
+        this.setState({ loading: false });
+      }
     render() {
+        if (this.state.loading) {
+            return <ActivityIndicator />
+          }
         return (
             <Container>
                 <Content>
